decb2c67a4a66866f471001989cb22be087a893a
Ayaan <75617112+Technogenair-Ayaan@users.noreply.github.com>
2020-12-10 09:37:16 +0530

    Python project
    It is a beginner form of calculator in python.

---
+++ All Operations.py
@@ -0,0 +1,15 @@
+print("Welcome to All Operations Program")
+print("Type your first number\t For subtraction bigger number \t For division dividend")
+first = input()
+print("Type your second number\t For subtraction smaller number \t For division divisor")
+sec = input()
+sum = int(first)+int(sec)
+sub = int(first)-int(sec)
+mul = int(first)*int(sec)
+div = int(first)/int(sec)
+print("The sum is:" , sum  )
+print("The difference is:",sub)
+print("The product is:",mul)
+print("The quotient is:",div)
+
+
